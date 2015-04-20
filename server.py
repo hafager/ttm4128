22033004e0b90d2cc9a7b3d62106c517337f1f39
Håvard Magne Fagervoll <fagervoll@gmail.com>
2015-04-20 13:49:46 +0200

    Makes it show the index page with the layout from layout.html

--- server.py
+++ server.py
@@ -1,14 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "post: os og ip interfaces dette blir kult"
 
-if __name__ == "__main__":
-    app.run("78.91.20.230")
+
 
 @app.route('/')
 def index():
     """Just a generic index page to show."""
     return render_template('index.html')
+
+if __name__ == "__main__":
+    app.run()
