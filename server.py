6edb72ffe002d907978f4c78a3f81a985f7e6720
maritrud <marit.rudlang@gmail.com>
2015-04-20 13:31:32 +0200

    la til min ip for aa teste

--- server.py
+++ server.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "post: os og ip interfaces"
+    return "post: os og ip interfaces dette blir kult"
 
 if __name__ == "__main__":
-    app.run()
+    app.run("78.91.20.230")
