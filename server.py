8d458120563b1997d331eea3aa35f5eba29df880
tuvasd <tuvas.dybedokken@gmail.com>
2015-04-20 14:11:27 +0200

    Update server.py

--- server.py
+++ server.py
@@ -1,9 +1,22 @@
 from flask import Flask
+from cim_client import ipInterface, os
+
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "post: os og ip interfaces"
 
-if __name__ == "__main__":
-    app.run()
+#Calls the os function
+operatingSystemInfo = os()
+
+#Calls the function to get the name, ip and mask
+name,ip,mask = ipInterface()
+
+#Tells Flask what URL should trigger our function.
+@app.route('/')
+def hello():
+	#Print the info (HER LEGGES DET TIL HTML)
+	return ("OS: " + operatingSystemInfo + '/n' + "Name: " + name + '/n' + "Ip: " + ip + '/n' + "Mask: " + mask)
+
+#Makes sure the server only runs if the script is executed directly from the Python interpreter and not used as an imported module. 
+if __name__ == '__main__':
+	#Tells the server the ip-address you can reach the server. (Formulere bedre?? :P )
+    app.run("78.91.37.242")
