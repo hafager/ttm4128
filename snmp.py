fd3a82847b3c2d4b6bbbae30fb57802819578713
maritrud <marit.rudlang@gmail.com>
2015-04-21 15:30:14 +0200

    kommentert kode

--- snmp.py
+++ snmp.py
@@ -6,6 +6,8 @@ from time import sleep
 def snmp_os():
     #Enters the command "snmpwalk -v 2c -c ttm4128 129.241.209.10 sysDescr" in terminal and returns the output.
     os = check_output(["snmpwalk", "-v", "2c", "-c", "ttm4128", "129.241.209.10", "sysDescr"]).splitlines()
+    
+    #removes the unecessary overhead to the os information for all oses on the given ip-address
     for i in range(len(os)):
         os[i] = " ".join(os[i].split()[3:])
     return os
@@ -20,17 +22,20 @@ def snmp_interface():
     #Enters the command "snmpwalk -v 2c -c ttm4128 129.241.209.10 ipAdEntNetMask" in terminal and stores it in mask-variable
     mask = check_output(["snmpwalk", "-v", "2c", "-c", "ttm4128", "129.241.209.10", "ipAdEntNetMask"]).splitlines()
     
+    #creating an array of arrays with three fields: [name, ip, mask]
     interface = []
 
     for n in range(len(name)):
-    	interface.append([name[n].split()[-1]])
+    	#stripping the unecessary information and adding the name field to the array
+        interface.append([name[n].split()[-1]])
 
     for i in range(len(ip)):
-    	address = ip[i].split()
-    	interface[i].append(address[-1])
+    	#stripping the unecessary information and adding the ip field to the array 
+    	interface[i].append(ip[i].split()[-1])
 
     for m in range(len(mask)):
-    	interface[m].append(mask[m].split()[-1])
+    	#stripping the unecessary information and adding the mask field to the array
+        interface[m].append(mask[m].split()[-1])
 
     return interface
 
