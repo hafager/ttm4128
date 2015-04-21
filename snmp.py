58ca798db9310e755b50922a6886d2a7886dd059
Frederik Nygaard <frederikny@gmail.com>
2015-04-21 15:19:46 +0200

    Added some comments

--- snmp.py
+++ snmp.py
@@ -2,20 +2,22 @@ from subprocess import call
 from subprocess import check_output
 from time import sleep
 
-
+#Method that returns 129.241.209.10s computers OS-info. 
 def snmp_os():
-
+    #Enters the command "snmpwalk -v 2c -c ttm4128 129.241.209.10 sysDescr" in terminal and returns the output.
     os = check_output(["snmpwalk", "-v", "2c", "-c", "ttm4128", "129.241.209.10", "sysDescr"]).splitlines()
     for i in range(len(os)):
         os[i] = " ".join(os[i].split()[3:])
     return os
 
 def snmp_interface():
-    
+    #Enters the command "snmpwalk -v 2c -c ttm4128 129.241.209.10 ifDescr" in terminal and stores it in name-variable
     name = check_output(["snmpwalk", "-v", "2c", "-c", "ttm4128", "129.241.209.10", "ifDescr"]).splitlines()
-
+    
+    #Enters the command "snmpwalk -v 2c -c ttm4128 129.241.209.10 ipAdEntAddr" in terminal and stores it in ip-variable
     ip = check_output(["snmpwalk", "-v", "2c", "-c", "ttm4128", "129.241.209.10", "ipAdEntAddr"]).splitlines()
     
+    #Enters the command "snmpwalk -v 2c -c ttm4128 129.241.209.10 ipAdEntNetMask" in terminal and stores it in mask-variable
     mask = check_output(["snmpwalk", "-v", "2c", "-c", "ttm4128", "129.241.209.10", "ipAdEntNetMask"]).splitlines()
     
     interface = []
